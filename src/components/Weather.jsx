b5b5db930105eabcdd620e00b63b9576fb9d319c
sadish panta <sadishpanta404@gmail.com>
2025-04-16 21:43:08 +0545

    the functionlaity of weather app added with onclick the searh bar after the input the user will get the weather data

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import search from '../assets/search.png'
 import clear from '../assets/clear.png'
 import cloud from '../assets/cloud.png'
@@ -11,20 +11,62 @@ import rain from '../assets/rain.png'
 
 const Weather = () => {
 
-    const search=async(city)=>{
+  const inputRef=useRef()
+const [weatherdata,Setweatherdata]=useState(false)
+
+const allicon={
+  "01d":clear,
+  "01n":clear,
+  "02d":cloud,
+  "02n":cloud,
+  "03d":cloud,
+  "03n":cloud,
+  "04d":drizzle,
+  "04n":drizzle,
+  "09d":rain,
+  "10d":rain,
+  "10n":rain,
+  "13d":snow,
+  "13n":snow,
+
+
+}
+
+
+    const look=async(city)=>{
+
+    if(city===""){
+      alert("Enter City Name")
+      return;
+    }
       try {
-        const url=` http://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_APP_ID}&q=${searchInput.value}`
+        const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`
 
         const response=await fetch(url);
-        const data=await response.JSON;
+        const data=await response.json();
+
+if(!response.ok){
+  alert(data.message);
+  return;
+}
+
         console.log(data)
+        const icon=allicon[data.weather[0].icon]|| clear
+        Setweatherdata({
+          humidity:data.main.humidity,
+          windSpeed: data.wind.speed,
+          temperature: Math.floor(data.main.temp),
+          location: data.name,
+          icon: icon
+        })
       } catch (error) {
-        
+        Setweatherdata(false)
+        console.log("Error while fetching user data")
       }
     }
 
     useEffect(()=>{
-search("London");
+look("London");
     },[])
 
     
@@ -33,18 +75,19 @@ search("London");
   return (
     <div className='self-center p-[40px] rounded-[10px] bg-linear-gradient weather' >
     <div className="search-bar">
-      <input type="text" placeholder='Search' />
-      <img src={search} alt=""  />
+      <input ref={inputRef} type="text" placeholder='Search' />
+      <img src={search} alt=""  onClick={()=>{look(inputRef.current.value)}} />
     </div>
-    <img src={clear} alt=""  className="weather-icon"/>
-    <p className="temperature">16°c</p>
-    <p className="location">London</p>
+    {weatherdata?<>
+      <img src={weatherdata.icon} alt=""  className="weather-icon"/>
+    <p className="temperature">{weatherdata.temperature}°c</p>
+    <p className="location">{weatherdata.location}</p>
     <div className="weather_data">
 
       <div className="col">
         <img src={humidity} alt="" />
         <div>
-          <p>91 %</p>
+          <p>{weatherdata.humidity} %</p>
           <span>Humidity</span>
         </div>
       </div>
@@ -52,13 +95,16 @@ search("London");
       <div className="col">
         <img src={wind} alt="" />
         <div>
-          <p>3.6 km/hr</p>
+          <p>{weatherdata.windSpeed} km/hr</p>
           <span>Wind Speed</span>
         </div>
       </div>
 
 
     </div>
+
+    </>:<></>}
+   
     </div>
   )
 }
